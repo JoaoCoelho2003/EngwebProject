e584b0836ceb2e1d963d990202f2d198dbaa997c
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2024-06-03 19:48:39 +0100

    refactor: path to images, again

--- scripts/xmlToJson.py
+++ scripts/xmlToJson.py
@@ -13,7 +13,7 @@ def get_figura(figuras):
     for child in figuras:
         if child.tag == "imagem":
             grupos = re.search(r'\.\.\/imagem\/(.*)', child.attrib["path"])
-            imagem ="MapaRuas-materialBase/imagens/" + grupos.group(1)
+            imagem ="MapaRuas-materialBase/imagem/" + grupos.group(1)
         elif child.tag == "legenda":
             legenda = child.text
 
