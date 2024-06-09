01b973432812c4848ef983cb35435755e254e753
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2024-06-09 19:18:32 +0100

    feat: moved the button to delete roads to the show page

--- lib/engweb_web/live/road_live/form_component.ex
+++ lib/engweb_web/live/road_live/form_component.ex
@@ -11,21 +11,35 @@ defmodule EngwebWeb.RoadLive.FormComponent do
         <%= @title %>
         <:subtitle>Use this form to manage road records in your database.</:subtitle>
       </.header>
-
-      <.simple_form
-        for={@form}
-        id="road-form"
-        phx-target={@myself}
-        phx-change="validate"
-        phx-submit="save"
-      >
-        <.input field={@form[:num]} type="number" label="Num" />
-        <.input field={@form[:name]} type="text" label="Name" />
-        <.input field={@form[:description]} type="text" label="Description" />
-        <:actions>
-          <.button phx-disable-with="Saving...">Save Road</.button>
-        </:actions>
+      <%= if @action == :delete do %>
+        <div class="flex justify-end">
+          <.simple_form
+            for={@form}
+            id="road-form"
+            phx-target={@myself}
+            phx-submit="delete"
+          >
+          <:actions>
+            <.button phx-disable-with="Deleting..." class="bg bg-red-600 hover:bg-red-700">Delete Road</.button>
+          </:actions>
       </.simple_form>
+      </div>
+      <% else %>
+        <.simple_form
+          for={@form}
+          id="road-form"
+          phx-target={@myself}
+          phx-change="validate"
+          phx-submit="save"
+        >
+          <.input field={@form[:num]} type="number" label="Num" />
+          <.input field={@form[:name]} type="text" label="Name" />
+          <.input field={@form[:description]} type="text" label="Description" />
+          <:actions>
+            <.button phx-disable-with="Saving...">Save Road</.button>
+          </:actions>
+      </.simple_form>
+      <% end %>
     </div>
     """
   end
@@ -54,33 +68,64 @@ defmodule EngwebWeb.RoadLive.FormComponent do
     save_road(socket, socket.assigns.action, road_params)
   end
 
+  def handle_event("delete", _params, socket) do
+    delete_road(socket)
+  end
+
   defp save_road(socket, :edit, road_params) do
-    case Roads.update_road(socket.assigns.road, road_params) do
-      {:ok, road} ->
-        notify_parent({:saved, road})
+    if socket.assigns.road.user_id != socket.assigns.current_user.id do
+      {:noreply, socket |> put_flash(:error, "You are not allowed to edit this road")}
+    else
+      case Roads.update_road(socket.assigns.road, road_params) do
+        {:ok, road} ->
+          notify_parent({:saved, road})
 
-        {:noreply,
-         socket
-         |> put_flash(:info, "Road updated successfully")
-         |> push_patch(to: socket.assigns.patch)}
+          {:noreply,
+          socket
+          |> put_flash(:info, "Road updated successfully")
+          |> push_patch(to: socket.assigns.patch)}
 
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign_form(socket, changeset)}
+        {:error, %Ecto.Changeset{} = changeset} ->
+          {:noreply, assign_form(socket, changeset)}
+      end
     end
   end
 
   defp save_road(socket, :new, road_params) do
-    case Map.put(road_params, "user_id", socket.assigns.current_user.id) |> Roads.create_road() do
-      {:ok, road} ->
-        notify_parent({:saved, road})
+    if socket.assigns.current_user.id == nil do
+      {:noreply, socket |> put_flash(:error, "You must be logged in to create a road")}
+    else
+      case Map.put(road_params, "user_id", socket.assigns.current_user.id) |> Roads.create_road() do
+        {:ok, road} ->
+          notify_parent({:saved, road})
 
-        {:noreply,
-         socket
-         |> put_flash(:info, "Road created successfully")
-         |> push_patch(to: socket.assigns.patch)}
+          {:noreply,
+          socket
+          |> put_flash(:info, "Road created successfully")
+          |> push_patch(to: socket.assigns.patch)}
 
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign_form(socket, changeset)}
+        {:error, %Ecto.Changeset{} = changeset} ->
+          {:noreply, assign_form(socket, changeset)}
+      end
+    end
+  end
+
+  defp delete_road(socket) do
+    if socket.assigns.road.user_id != socket.assigns.current_user.id do
+      {:noreply, socket |> put_flash(:error, "You are not allowed to delete this road")}
+    else
+      case Roads.delete_road(socket.assigns.road) do
+        {:ok, _} ->
+          notify_parent({:deleted, socket.assigns.road})
+
+          {:noreply,
+          socket
+          |> put_flash(:info, "Road deleted successfully")
+          |> push_navigate(to: socket.assigns.patch)}
+
+        {:error, _} ->
+          {:noreply, socket |> put_flash(:error, "Error deleting road")}
+      end
     end
   end
 
